c759af64cb6f61364f7e8ab218109c763a3adbc6
levuloihust99 <49064246+levuloihust99@users.noreply.github.com>
2026-05-10 22:19:32 +0700

    update search options

--- loilv_file_search/search_files.py
+++ loilv_file_search/search_files.py
@@ -1,10 +1,17 @@
 import os
 import json
+import copy
 import logging
 import argparse
+
 from typing import Text, Optional, List, Union
 
-from .utils import check_for_ignore, search_pattern_in_file, format_print_path
+from .utils import (
+    check_for_ignore,
+    search_pattern_in_file,
+    format_print_path,
+    MatchingMode,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -14,12 +21,14 @@ def scan_files(
     search_pattern: Union[Text, List[Text]],
     ignore_pattern: Optional[Union[Text, List[Text]]] = None,
     include_pattern: Optional[Union[Text, List[Text]]] = None,
-    include_over_ignore: bool = True,
+    ignore_file_pattern: Optional[Union[Text, List[Text]]] = None,
+    include_file_pattern: Optional[Union[Text, List[Text]]] = None,
+    matching_mode: MatchingMode = MatchingMode.INCLUDE_OVERRIDES_IGNORE,
 ) -> List[Text]:
     logger.info("Scanning...")
     root_path_dir = os.path.basename(root_path)
     is_ignore = check_for_ignore(
-        root_path_dir, ignore_pattern, include_pattern, include_over_ignore
+        root_path_dir, ignore_pattern, include_pattern, matching_mode
     )
     if is_ignore:
         return
@@ -34,15 +43,29 @@ def scan_files(
         if os.path.isdir(path):
             subpaths = os.listdir(path)
             for f in subpaths:
+                resolved_include_pattern = include_pattern
+                resolved_ignore_pattern = ignore_pattern
+
+                f_path = os.path.join(path, f)
+                if os.path.isfile(f_path):
+                    resolved_include_pattern = include_file_pattern or include_pattern
+                    resolved_ignore_pattern = ignore_file_pattern or ignore_pattern
+
                 is_ignore = check_for_ignore(
-                    f, ignore_pattern, include_pattern, include_over_ignore
+                    f,
+                    ignore_pattern=resolved_ignore_pattern,
+                    include_pattern=resolved_include_pattern,
+                    matching_mode=matching_mode,
                 )
                 if is_ignore is True:
                     continue
                 stack.append(os.path.join(path, f))
         else:
-            found = search_pattern_in_file(path, search_pattern)
-            if found is True:
+            is_matching = True
+            if search_pattern:
+                is_matching = search_pattern_in_file(path, search_pattern)
+
+            if is_matching is True:
                 queue.append(path)
 
     print(printing_prefix)
@@ -58,34 +81,87 @@ def scan_files(
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", "-i", default=os.getcwd())
-    parser.add_argument("--ignore_pattern", "-g", nargs="+", default=None)
-    parser.add_argument("--include_pattern", "-c", nargs="+", default=None)
-    parser.add_argument("--search_pattern", "-s", nargs="+", default=None)
-    parser.add_argument("--include_over_ignore", "-y", default=True, type=eval)
+    parser.add_argument(
+        "--ignore_pattern",
+        "-g",
+        nargs="+",
+        default=None,
+        help=(
+            "Ignore patterns for any folders/files during traversing. "
+            "If a folder matches this pattern, all its subdirectories/files are ignored immediately (no further search)."
+        ),
+    )
+    parser.add_argument(
+        "--include_pattern",
+        "-c",
+        nargs="+",
+        default=None,
+        help=(
+            "Include patterns for any folders/files during traversing. "
+            "If a folder matches this pattern, the search will go deeper into its files/subdirectories."
+        ),
+    )
+    parser.add_argument(
+        "--include_file_pattern",
+        nargs="+",
+        default=None,
+        help="Include pattern for files only. Does not apply to directories.",
+    )
+    parser.add_argument(
+        "--ignore_file_pattern",
+        nargs="+",
+        default=None,
+        help="Ignore pattern for files only. Does not apply to directories.",
+    )
+    parser.add_argument(
+        "--search_pattern",
+        "-s",
+        nargs="+",
+        default=None,
+        help="Search for patterns in file content. Only apply for text files. Binary files are skipped.",
+    )
+    parser.add_argument(
+        "--matching_mode",
+        choices=[
+            MatchingMode.INCLUDE_OVERRIDES_IGNORE,
+            MatchingMode.IGNORE_OVERRIDES_INCLUDE,
+            MatchingMode.INCLUDE_AND_IGNORE_BOTH_SATISFIED,
+        ],
+        default="include_overrides_ignore",
+        help="Specify how to check if a file is matched when both include and ignore patterns are present.",
+    )
     parser.add_argument("--pattern_file", "-p", default=None)
     args = parser.parse_args()
 
-    if args.search_pattern is None:
-        logger.warning("You didn't pass the 'search_pattern' param. This is noop.")
+    params = {}
     if args.pattern_file:
         with open(args.pattern_file, "r") as reader:
             pattern_rule = json.load(reader)
-        search_pattern = pattern_rule.get("search_pattern", None)
-        if search_pattern:
-            args.search_pattern = search_pattern
-        ignore_pattern = pattern_rule.get("ignore_pattern", None)
-        if ignore_pattern:
-            args.ignore_pattern = ignore_pattern
-        include_pattern = pattern_rule.get("include_pattern", None)
-        if include_pattern:
-            args.include_pattern = include_pattern
+        params["search_pattern"] = pattern_rule.get("search_pattern")
+        params["ignore_pattern"] = pattern_rule.get("ignore_pattern")
+        params["include_pattern"] = pattern_rule.get("include_pattern")
+        params["ignore_file_pattern"] = pattern_rule.get("ignore_file_pattern")
+        params["include_file_pattern"] = pattern_rule.get("include_file_pattern")
+
+    args_json = copy.deepcopy(args.__dict__)
+    for k, v in args_json.items():
+        if k in {
+            "search_pattern",
+            "ignore_pattern",
+            "include_pattern",
+            "ignore_file_pattern",
+            "include_file_pattern",
+        }:
+            params[k] = v
 
     scan_files(
         root_path=args.input_path,
-        search_pattern=args.search_pattern,
-        ignore_pattern=args.ignore_pattern,
-        include_pattern=args.include_pattern,
-        include_over_ignore=args.include_over_ignore,
+        search_pattern=params["search_pattern"],
+        ignore_pattern=params["ignore_pattern"],
+        include_pattern=params["include_pattern"],
+        ignore_file_pattern=params["ignore_file_pattern"],
+        include_file_pattern=params["include_file_pattern"],
+        matching_mode=args.matching_mode,
     )
 
 
--- loilv_file_search/utils.py
+++ loilv_file_search/utils.py
@@ -1,13 +1,22 @@
 import re
-from typing import Text, Optional, List, Union
 
+from enum import Enum
+from typing import Text, Optional, List, Union
 
 # https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
 textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F})
 is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
 
 
-def pattern_matching(pattern: Optional[Union[Text, List[Text]]], text: Text, default: bool = True):
+class MatchingMode(str, Enum):
+    INCLUDE_OVERRIDES_IGNORE = "include_overrides_ignore"
+    IGNORE_OVERRIDES_INCLUDE = "ignore_overrides_include"
+    INCLUDE_AND_IGNORE_BOTH_SATISFIED = "include_and_ignore_both_satisfied"
+
+
+def pattern_matching(
+    pattern: Optional[Union[Text, List[Text]]], text: Text, default: bool = True
+):
     if pattern is None:
         return bool(default)
     if not isinstance(pattern, list):
@@ -20,7 +29,7 @@ def pattern_matching(pattern: Optional[Union[Text, List[Text]]], text: Text, def
 
 
 def search_pattern_in_file(file_path: Text, pattern: Union[Text, List[Text]]):
-    with open(file_path, 'rb') as reader:
+    with open(file_path, "rb") as reader:
         file_opening = reader.read(1024)
     is_binary = is_binary_string(file_opening)
     if is_binary is True:
@@ -38,17 +47,27 @@ def check_for_ignore(
     path_dir: Text,
     ignore_pattern: Optional[Union[Text, List[Text]]] = None,
     include_pattern: Optional[Union[Text, List[Text]]] = None,
-    include_over_ignore: bool = True
-):
+    matching_mode: MatchingMode = MatchingMode.INCLUDE_OVERRIDES_IGNORE,
+) -> bool:
+    if ignore_pattern is None and include_pattern is None:
+        return False
+
+    if ignore_pattern is None:
+        return not pattern_matching(include_pattern, path_dir, default=False)
+
+    if include_pattern is None:
+        return pattern_matching(ignore_pattern, path_dir, default=False)
+
+    # both `include_pattern` and `ignore_pattern` is not None
     is_ignore = False
-    if include_over_ignore:
+    if matching_mode == MatchingMode.INCLUDE_OVERRIDES_IGNORE:
         should_ignore = pattern_matching(ignore_pattern, path_dir, default=False)
         if should_ignore is True:
             is_ignore = True
             should_include = pattern_matching(include_pattern, path_dir, default=False)
             if should_include is True:
                 is_ignore = False
-    else:
+    elif matching_mode == MatchingMode.IGNORE_OVERRIDES_INCLUDE:
         should_include = pattern_matching(include_pattern, path_dir, default=False)
         if should_include is True:
             should_ignore = pattern_matching(ignore_pattern, path_dir, default=False)
@@ -56,8 +75,13 @@ def check_for_ignore(
                 is_ignore = True
         else:
             is_ignore = True
-    return is_ignore
+    else:
+        # both include and ignore patterns need to be satisfied
+        should_ignore = pattern_matching(ignore_pattern, path_dir, default=False)
+        should_include = pattern_matching(include_pattern, path_dir, default=False)
+        is_ignore = (not should_include) or should_ignore
 
+    return is_ignore
 
 
 def format_print_path(path: Text, max_line_len: int = -1):
